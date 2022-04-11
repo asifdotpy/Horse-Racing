13bcbf8c45c4b7e38c40462db0ce2f3f6ba34431
asifdotpy <asifdotpy@gmail.com>
2022-04-11 23:27:05 +0600

    Added button for selection

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ Date: 30/04/17
 */
 
 /*Create a Javascript Object for a horse with 3 parameters: HTML ID, position x and y*/
-function Horse(id, x, y){
+function Horse(id, x, y) {
 	this.element = document.getElementById(id);/*HTML element of the horse*/
-	this.speed = Math.random()*10 + 10; /*Initiate a random speed for each horse, the greater speed, the faster horse. The value is between 10 and 20*/
+	this.speed = Math.random() * 10 + 10; /*Initiate a random speed for each horse, the greater speed, the faster horse. The value is between 10 and 20*/
 	this.originX = x;/*Original X position*/
 	this.originY = y;/*Original Y position*/
 	this.x = x; /*Current X*/
@@ -16,89 +16,89 @@ function Horse(id, x, y){
 	this.number = parseInt(id.replace(/[\D]/g, '')); /*Number of horse, number will be 1 or 2 or 3 or 4*/
 	this.lap = 0; //Current lap of the horse
 
-	this.moveRight = function(){
+	this.moveRight = function () {
 		var horse = this;/*Assign horse to this object*/
 
 		/*Use setTimeout to have the delay in moving the horse*/
-		setTimeout(function(){
+		setTimeout(function () {
 			//Move the horse to right 1vw
-			horse.x ++;
-			horse.element.style.left = horse.x +'vw';
+			horse.x++;
+			horse.element.style.left = horse.x + 'vw';
 
 			//Check if goes through the start line, if horse runs enough number of laps and has pass the start line then stop
-			if (horse.lap == num_lap && horse.x > horse.originX + 6){
+			if (horse.lap == num_lap && horse.x > horse.originX + 6) {
 				horse.arrive();
-			}else{
+			} else {
 				//Make decision to move Down or not
 				//The width of the Down Road is 10wh, then the distance of each horse is 2.5vw (4 horses). The right position of the road is 82.5vw
 				//Continue to move right if not reach the point to turn
-				if (horse.x < 82.5 - horse.number*2.5){
+				if (horse.x < 82.5 - horse.number * 2.5) {
 					horse.moveRight();
-				}else{
+				} else {
 					//Change HTML class of horse to runDown
 					horse.element.className = 'horse runDown';
 					//Change the speed, will be random value from 10 to 20
-					horse.speed = Math.random()*10 + 10;
+					horse.speed = Math.random() * 10 + 10;
 					horse.moveDown();
 				}
 			}
 
-		}, 1000/this.speed);
+		}, 1000 / this.speed);
 		/* 1000/this.speed is timeout time*/
 	}
 
 	/*Do the same for moveDown, moveLeft, moveUp*/
-	this.moveDown = function(){
+	this.moveDown = function () {
 		var horse = this;
-		setTimeout(function(){
-			horse.y ++;
-			horse.element.style.top = horse.y +'vh';
-			if (horse.y < horse.originY + 65){
+		setTimeout(function () {
+			horse.y++;
+			horse.element.style.top = horse.y + 'vh';
+			if (horse.y < horse.originY + 65) {
 				horse.moveDown();
-			}else{
+			} else {
 				horse.element.className = 'horse runLeft';
-				horse.speed = Math.random()*10 + 10;
+				horse.speed = Math.random() * 10 + 10;
 				horse.moveLeft();
 			}
-		}, 1000/this.speed)
+		}, 1000 / this.speed)
 	}
-	this.moveLeft = function(){
+	this.moveLeft = function () {
 		var horse = this;
-		setTimeout(function(){
-			horse.x --;
-			horse.element.style.left = horse.x +'vw';
-			if (horse.x > 12.5 - horse.number*2.5){
+		setTimeout(function () {
+			horse.x--;
+			horse.element.style.left = horse.x + 'vw';
+			if (horse.x > 12.5 - horse.number * 2.5) {
 				horse.moveLeft();
-			}else{
+			} else {
 				horse.element.className = 'horse runUp';
-				horse.speed = Math.random()*10 + 10;
+				horse.speed = Math.random() * 10 + 10;
 				horse.moveUp();
 			}
-		}, 1000/this.speed)
+		}, 1000 / this.speed)
 	}
-	this.moveUp = function(){
+	this.moveUp = function () {
 		var horse = this;
-		setTimeout(function(){
-			horse.y --;
-			horse.element.style.top = horse.y +'vh';
-			if (horse.y > horse.originY){
-				horse.speed = Math.random()*10 + 10;
+		setTimeout(function () {
+			horse.y--;
+			horse.element.style.top = horse.y + 'vh';
+			if (horse.y > horse.originY) {
+				horse.speed = Math.random() * 10 + 10;
 				horse.moveUp();
-			}else{
+			} else {
 				horse.element.className = 'horse runRight';
 				//Nearly finish the lap
-				horse.lap ++;
+				horse.lap++;
 				horse.moveRight();
 			}
-		}, 1000/this.speed)
+		}, 1000 / this.speed)
 	}
 
 	/*Trigger the horse by run*/
-	this.run = function(){
+	this.run = function () {
 		this.element.className = 'horse runRight';
 		this.moveRight();
 	}
-	this.arrive = function(){
+	this.arrive = function () {
 		//Stop the horse run by change class to standRight
 		this.element.className = 'horse standRight';
 		this.lap = 0;//Reset the lap
@@ -106,21 +106,21 @@ function Horse(id, x, y){
 		/*Show the result*/
 		var tds = document.querySelectorAll('#results .result');//Get all table cell to display the result
 		//results.length is the current arrive position
-		tds[results.length].className = 'result horse'+this.number;//The class of result look like: result horse1...
+		tds[results.length].className = 'result horse' + this.number;//The class of result look like: result horse1...
 
 		//Push the horse number to results array, according the the results array, we know the order of race results
 		results.push(this.number);
 
 		//Win horse
-		if (results.length == 1){
+		if (results.length == 1) {
 			//If win horse is the bet horse, then add the fund
-			if (this.number == bethorse){
+			if (this.number == bethorse) {
 				funds += amount;
-			}else{
+			} else {
 				funds -= amount;
 			}
 			document.getElementById('funds').innerText = funds;
-		}else if (results.length == 4){
+		} else if (results.length == 4) {
 			//All horse arrived, enable again the Start Button
 			document.getElementById('start').disabled = false;
 		}
@@ -130,7 +130,7 @@ function Horse(id, x, y){
 var num_lap = 1, results = [], funds = 100, bethorse, amount;
 
 //Start the function when the document loaded
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function (event) {
 
 	var horse1 = new Horse('horse1', 20, 4);
 	var horse2 = new Horse('horse2', 20, 8);
@@ -138,17 +138,17 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	var horse4 = new Horse('horse4', 20, 16);
 
 	//Event listener to the Start button
-	document.getElementById('start').onclick = function(){
+	document.getElementById('start').onclick = function () {
 		amount = parseInt(document.getElementById('amount').value);
 		num_lap = parseInt(document.getElementById('num_lap').value);
 		bethorse = parseInt(document.getElementById('bethorse').value);
 
-		if (funds < amount){
+		if (funds < amount) {
 			alert('Not enough funds.');
 		}
-		else if (num_lap <= 0){
+		else if (num_lap <= 0) {
 			alert('Number of lap must be greater than 1.');
-		}else{
+		} else {
 
 			/*Started the game*/
 			this.disabled = true;/*Disable the start button*/
@@ -166,3 +166,20 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		}
 	}
 });
+
+// Input button function
+
+function increaseValue() {
+	var value = parseInt(document.getElementById('number').value, 10);
+	value = isNaN(value) ? 0 : value;
+	value++;
+	document.getElementById('number').value = value;
+}
+
+function decreaseValue() {
+	var value = parseInt(document.getElementById('number').value, 10);
+	value = isNaN(value) ? 0 : value;
+	value < 1 ? value = 1 : '';
+	value--;
+	document.getElementById('number').value = value;
+}
