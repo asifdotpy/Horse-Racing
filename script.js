14a56f4e75c0de78522e76f810a3eaf07159c09e
asifdotpy <asifdotpy@gmail.com>
2022-04-13 00:49:06 +0600

    Added active css into white and blue button

--- script.js
+++ script.js
@@ -204,7 +204,23 @@ function startTimer(duration, display) {
 }
 
 window.onload = function () {
-	var fiveMinutes = 60 * 5,
-		display = document.querySelector('#countdownTimer');
-	startTimer(fiveMinutes, display);
+	var fiveMinutes = 60 * 5;
+	var tenSeconds = 10;
+	display = document.querySelector('#countdownTimer');
+	startTimer(tenSeconds, display);
+	animalSelect();
 };
+
+
+async function animalSelect() {
+	// Add active class to the current button (highlight it)
+	var animalSelect = await document.getElementById("animalSelect");
+	var btns = await animalSelect.getElementsByClassName("bn28");
+	for (var i = 0; i < btns.length; i++) {
+		btns[i].addEventListener("click", function () {
+			var current = document.getElementsByClassName("active");
+			current[0].className = current[0].className.replace(" active", "");
+			this.className += " active";
+		});
+	}
+}
